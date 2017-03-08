05bbbc49af8634f9c6508f5e1fb018a43ef0e109
wesareed <wes@wesreed.co>
2017-03-08 14:52:22 -0500

    applies custome styles to menu

--- wp-content/themes/humescores/functions.php
+++ wp-content/themes/humescores/functions.php
@@ -45,7 +45,7 @@ function humescores_setup()
 
     // This theme uses wp_nav_menu() in one location.
     register_nav_menus(array(
-        'menu-1' => esc_html__('Primary', 'humescores'),
+        'menu-1' => esc_html__('Header', 'humescores'),
     ));
 
     /*
